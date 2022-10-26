7f253da22b6b2ebe261d3c1b9e210a5ed197f5af
YYK-YANG <2819136900@qq.com>
2022-10-26 14:00:59 +0800

    Modify main.c

--- task1/main.c
+++ task1/main.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
-	printf("Hello World!\n");
+
+int main (void)
+{
+	double x = 0;
+	double y = 0;
+	int a = y;
+	printf ("请输入初始值a\n");
+	scanf ("%lf",&y);
+	printf ("x        y\n");
+	
+for (a = y;y - y*0.999000>=0.000001;x = x+0.001)
+	{
+    printf ("%f       %f\n",x,y);
+    y = y*0.999000;
+	}
+               
+	printf ("%f       %f\n",x,y);
+
+	
+	printf ("最终定态为：%lf",y);
 	return 0;
 }
 
